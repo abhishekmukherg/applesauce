59730fbd928a7129322fb38e22c76cd075fef7a5
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-30 19:49:03 +0000

    Revert "Added util.py from the gummy_panzer project"
    This reverts commit a8de4d34de6f2c033447a18919bee82ec8b07acb.
    
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/applesauce/trunk@1400 aaafb41a-2973-0410-b970-db74e9e91ae2

--- applesauce/sprite/player.py
+++ applesauce/sprite/player.py
@@ -10,3 +10,28 @@ class Player(pygame.sprite.Sprite):
         # TODO: defind self.image, self.rect
         self.movement = {'up':False, 'down':False, 'left':False, 'right':False}
         self.speed = 5
+        
+    def set_movement(self, direction):
+        """Sets the movement direction
+
+        Removes movement on all directions except "direction". If direction is
+        a list, then set to those directions. If direction is None, no movement
+        is done
+
+        """
+        if direction is None: # no movement
+            self.__cancel_movement()
+        elif isinstance(direction, collections.Iterable):
+            self.__set_movement(direction)
+        else:
+            self.__set_movement([direction])
+
+    def __cancel_movement(self):
+        for k in self.movement:
+            self.movement[k] = False
+
+    def __set_movement(self, directions):
+        self.__cancel_movement()
+        for direction in directions:
+            assert direction in self.movement
+            self.movement[dir] = True
