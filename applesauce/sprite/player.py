61ba4e59fedb8d2b81254f0fbc5e3e332e371120
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-30 19:15:58 +0000

    Twiddles with player.py a bit
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/applesauce/trunk@1362 aaafb41a-2973-0410-b970-db74e9e91ae2

--- applesauce/sprite/player.py
+++ applesauce/sprite/player.py
@@ -1,20 +1,36 @@
-import pygame, sprite
+import collections
 
-class Player( object ):
+import pygame
 
+class Player(pygame.sprite.Sprite):
     
-    def __init__( self ):
+    def __init__(self, *groups):
+        pygame.sprite.Sprite.__init__(self, *groups)
+        # TODO: defind self.image, self.rect
         self.movement = {'up':False, 'down':False, 'left':False, 'right':False}
-        #self.sprite = sprite.Sprite( 1, 10, pygame.image.load( 'images/' ).conver_alpha() )
         self.speed = 5
         
-        
-    def set_movement( self, direction, value ):
-        self.movement[direction] = value
-        
-        
-    #def update( self ):
-        
-        
-    def draw( self, screen ):
-        self.sprite.draw()
+    def set_movement(self, direction):
+        """Sets the movement direction
+
+        Removes movement on all directions except "direction". If direction is
+        a list, then set to those directions. If direction is None, no movement
+        is done
+
+        """
+        if direction is None: # no movement
+            self.__cancel_movement()
+        elif isinstance(direction, collections.Iterable):
+            self.__set_movement(direction)
+        else:
+            self.__set_movement([direction])
+
+    def __cancel_movement(self):
+        for k in self.movement:
+            self.movement[k] = False
+
+    def __set_movement(self, directions):
+        self.__cancel_movement()
+        for direction in directions:
+            assert direction in self.movement
+            self.movement[dir] = True
