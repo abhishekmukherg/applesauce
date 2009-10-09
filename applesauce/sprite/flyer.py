32665244b06fe8e14a56cb54b9326e4ab692acc6
coppon <coppon@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-10-09 16:47:03 +0000

    Forgot to add the flyer.py file. Adding the empty turkeyshake.py file so I don't forget later.
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/applesauce/trunk@1963 aaafb41a-2973-0410-b970-db74e9e91ae2

---
+++ applesauce/sprite/flyer.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class Flyer(pygame.sprite.Sprite):
+    
+    def __init__(self, location, direction, *groups):
+        pygame.sprite.Sprite.__init__( self, *groups )
+        if direction == 'up':
+            self.image = pygame.Surface( ( 10, 3 ) )
+        elif direction == 'down':
+            self.image = pygame.Surface( ( 10, 3 ) )
+        elif direction == 'left':
+            self.image = pygame.Surface( ( 3, 10 ) )
+        elif direction == 'right':
+            self.image = pygame.Surface( ( 3, 10 ) )
+        self.image.fill( ( 0, 255, 255 ) )
+        self.rect = self.image.get_rect()
+        
+        self.rect.center = location
+        
+    def update(self):
+        pass
+    
+    def draw(self, screen):
+        screen.blit( self.image, self.rect )
