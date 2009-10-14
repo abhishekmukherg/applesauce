3401e70c05e1d82a57deef7443e0e00bfd2cfd14
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-10-14 08:08:35 +0000

    Added door.py
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/applesauce/trunk@2754 aaafb41a-2973-0410-b970-db74e9e91ae2

---
+++ applesauce/sprite/door.py
@@ -0,0 +1,67 @@
+import weakref
+import logging
+
+import pygame
+
+from applesauce import settings
+from applesauce.sprite import util
+from applesauce.sprite import effects
+
+
+LOG = logging.getLogger(__name__)
+
+
+class Door(effects.SpriteSheet):
+    
+    def __init__(self, loc, horizontal, *groups):
+        self.type = 'door'
+        if horizontal:
+            image = util.load_image("Lvl_02_DoorH_Close_sheet.png")
+            size = (93, 31)
+        else:
+            image = util.load_image("Lvl_02_DoorV_Close_sheet.png")
+            size = (31, 93)
+        effects.SpriteSheet.__init__(self, image, size)
+        self.frame = 0
+        self.sound = None
+        self._moving = False
+        self.frame_mod = 1
+        
+        self.rect.center = loc
+
+    @property
+    def open(self):
+        return self.anim_frame == 0
+
+    @property
+    def moving(self):
+        return self._moving
+
+    @moving.setter
+    def moving(self, val):
+        if val != self._moving and val:
+            self.anim_frame += self.frame_mod
+        self._moving = val
+        
+    def update(self):
+        if not self.moving:
+            return
+        LOG.debug("Frame for door %d. anim_frame %d" % (self.frame,
+            self.anim_frame))
+        self.frame += 1
+
+        if self.frame < settings.DOOR_FRAME_TIME:
+            return
+
+        self.frame = 0
+        if self.anim_frame >= settings.DOOR_FRAME_COUNT - 1:
+            self.frame_mod = -1
+            self.moving = False
+        elif self.anim_frame <= 0:
+            self.frame_mod = 1
+            self.moving = False
+        else:
+            self.anim_frame += self.frame_mod
+
+        assert self.anim_frame < settings.DOOR_FRAME_COUNT and \
+                self.anim_frame >= 0
